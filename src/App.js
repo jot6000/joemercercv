b168ffdced5a2205cb22e84bb41442192d27e1d4
jot6000 <joemercer50@googlemail.com>
2022-06-12 18:58:42 +0100

    Small refactor and heading update

--- src/App.js
+++ src/App.js
@@ -3,111 +3,48 @@ import EducationCard from './EducationCard';
 import WorkCard from './WorkCard';
 import climb from './images/joeClimbing.png';
 import chess from './images/joeChess.png';
+import Heading from './features/Heading';
+import SkilsAndStatement from './features/SkillsAndStatement';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Joe Mercer
-        </h1>
-        <div className="Header-details">
-          <div/>
-          <div>
-            joemercer50@googlemail.co.uk
-          </div>
-          <div>
-            https://github.com/jot6000
-          </div>
-          <div/>
-        </div>
-      </header>
+      <Heading/>
       <body className="App-body">
-        <div className="Row1">
-          <div className="Row-item-primary">
-            <h1>
-              Technical skills
-            </h1>
-            <div className="List-segragation">
-              <ul>
-                <li>React</li>
-                <li>Javascript</li>
-                <li>Typescript</li>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Node.js</li>
-                <li>Visual Basic</li>
-                <li>C++</li>
-                <li>C#</li>
-              </ul>
-              <ul>
-                <li>Unity</li>
-                <li>Unreal Engine</li>
-                <li>Maya</li>
-                <li>Kofax Capture</li>
-                <li>Kofax KTM</li>
-                <li>UIpath</li>
-                <li>Kapow</li>
-                <li>SQL</li>
-                <li>Xamarin Forms</li>
-              </ul>
-            </div>
-          </div>
-          <div className="Row-item-secondary">
-            <h1>
-              Personal Statement
-            </h1>
-            <p>
-            Working independently, as a leader, or as an employee, I always give my all to achieve the desired goal. Throughout my working life I have been able to take on heavy workloads and still achieve high standard results consistently. A trait I am very proud of. From working in a range of environments I have had plenty of opportunities to refine my communication and team working skills. Which I feel creates the foundations of any professional relationships. My time working in industry and on startups has allowed me to gain a healthy amount of knowledge in a variety of different technical environments. Which has allowed me to quickly adapt when moving between tech stacks or job roles.
-            </p>
-          </div>
-          <div className="Row-item-primary">
-            <h1>
-              Soft skills
-            </h1>
-            <div className="List-segragation">
-              <ul>
-                <li>Adaptive</li>
-                <li>Communication</li>
-                <li>Cooperation</li>
-                <li>Creativity</li>
-                <li>Dependability</li>
-                <li>Handling pressure</li>
-                <li>Leadership</li>
-                <li>Organisation/Planning</li>
-                <li>Problem-solving</li>
-                <li>Time Keeping</li>   
-              </ul>               
-            </div>
-          </div>
-        </div>       
-        <h1>
-          Education
-        </h1>
-        <div className="Edu-cards">
-          <EducationCard title="Kapow 10 Technical Solutions Specialist" grade="Certified" location="Kofax" time ="2017" headColor="#388697"/>
-          <EducationCard title="RPA Developer Foundation Diploma" grade="Certified" location="UI Path" time ="2017" headColor="#388697"/>
-          <EducationCard title="Level 5 HND (Games Dev)" grade="Distinction" location="Uxbridge college" time ="Oct 2015 - Jul 2017" headColor="#388697"/>
-          <EducationCard title="Level 3 BTEC (Games Dev)" grade="Distinction*, Distinction, Distinction" location="Uxbridge college" time ="Sept 2013 - Jun 2015" headColor="#388697"/>
-          <EducationCard title="GCSEs" grade="10 GCSEs A-C" location="Chalfont Community College" time ="Sept 2011 - Jun 2013" headColor="#388697"/>
-        </div>
+        <SkilsAndStatement/>
         <h1>
           Work Experience
         </h1>
         <div className="Edu-cards">
+          <WorkCard title="Misc Start Ups" about={<ul>
+            <h4>Front End/Full stack Developer</h4>
+            <div>Responsibilities:</div>
+            <li>Building responsive React front ends.</li>
+            <li>Managing projects.</li>
+            <li>End to end and unit testing.</li>
+            <li>Designing full stack solutions.</li>
+            <li>System support.</li>
+          </ul>} time ="Oct 2018 - Jan 2019 | May 2021 - Now" headColor="#388697"/>
+          <WorkCard title="Uxbridge College" about={<ul>
+            <h4>Casual Lecturer - Comp Sci</h4>
+            <div>Responsibilities:</div>
+            <li>Teaching stsudents - Primarily web dev.</li>
+            <li>Planning schemes of work and assignments.</li>
+            <li>Marking assignments.</li>
+          </ul>} time ="Aug 2021 - Now" headColor="#388697"/>
           <WorkCard title="@Home Pizza" about={<ul>
             <h4>Delivery Driver</h4>
             <div>Responsibilities:</div>
             <li>Delivering pizza in a timely and professional manner.</li>
             <li>Professionally and politely take orders.</li>
-            </ul>} time ="Sept 2018 - Now" headColor="#388697"/>
+          </ul>} time ="Sept 2018 - Now" headColor="#388697"/>
           <WorkCard title="Hermes" about={<ul>
             <h4>Courier</h4>
             <div>Responsibilities:</div>
             <li>Delivering parcels with tight daily deadlines.</li>
             <li>Managing my daily schedule.</li>
             <li>Building positive relationships with the customers.</li>
-            </ul>} time ="Jan 2020 - Jun 2021" headColor="#388697"/>
+          </ul>} time ="Jan 2020 - Jun 2021" headColor="#388697"/>
           <WorkCard title="DCS" about={<ul>
             <h4>Software Engineer</h4>
             <div>Responsibilities:</div>
@@ -118,9 +55,19 @@ function App() {
             <li>Working both independently and in a team.</li>
             <li>Software and hardware installations for clients.</li>
             <li>Deploying databases.</li>
-            </ul>} time ="Oct 2017 - Oct 2018" headColor="#388697"/>
+          </ul>} time ="Oct 2017 - Oct 2018" headColor="#388697"/>
           <WorkCard title="Britannia Parking" about={<h4>Parking Officer</h4>} time ="Sept 2016 - Oct 2017" headColor="#388697"/>
           <WorkCard title="Dominoe's" about={<h4>Customer Service Representative</h4>} time ="Feb 2014 - Aug 2016" headColor="#388697"/>
+        </div>
+        <h1>
+          Certifications
+        </h1>
+        <div className="Edu-cards">
+          <EducationCard title="Kapow 10 Technical Solutions Specialist" grade="Certified" location="Kofax" time ="2017" headColor="#388697"/>
+          <EducationCard title="RPA Developer Foundation Diploma" grade="Certified" location="UI Path" time ="2017" headColor="#388697"/>
+          <EducationCard title="Level 5 HND (Games Dev)" grade="Distinction" location="Uxbridge college" time ="Oct 2015 - Jul 2017" headColor="#388697"/>
+          <EducationCard title="Level 3 BTEC (Games Dev)" grade="Distinction*, Distinction, Distinction" location="Uxbridge college" time ="Sept 2013 - Jun 2015" headColor="#388697"/>
+          <EducationCard title="GCSEs" grade="10 GCSEs A-C" location="Chalfont Community College" time ="Sept 2011 - Jun 2013" headColor="#388697"/>
         </div>
         <h1>
           About Me
---
+++ src/features/SkillsAndStatement.js
@@ -0,0 +1,65 @@
+export default function SkilsAndStatement(props){
+    return(
+        <div className="Row1">
+          <div className="card">
+            <h1>
+              Technical skills
+            </h1>
+            <div className="List-segragation">
+              <ul>
+                <li>React</li>
+                <li>Javascript</li>
+                <li>Typescript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Node.js</li>
+                <li>Visual Basic</li>
+                <li>C++</li>
+                <li>C#</li>
+                <li>CI/CD</li>
+              </ul>
+              <ul>
+                <li>Unity</li>
+                <li>Unreal Engine</li>
+                <li>Maya</li>
+                <li>Kofax Capture</li>
+                <li>Kofax KTM</li>
+                <li>UIpath</li>
+                <li>Kapow</li>
+                <li>SQL</li>
+                <li>Xamarin Forms</li>
+                <li>Jest</li>
+              </ul>
+            </div>
+          </div>
+          <div className="card wide">
+            <h1>
+              Personal Statement
+            </h1>
+            <p>
+              Working independently, as a leader, or as an employee, I always give my all to achieve the desired goal. Throughout my working life I have been able to take on heavy workloads and still achieve high standard results consistently. A trait I am very proud of. From working in a range of environments I have had plenty of opportunities to refine my communication and team working skills. Which I feel creates the foundations of any professional relationships. My time working in industry and on startups has allowed me to gain a healthy amount of knowledge in a variety of different technical environments. Which has allowed me to quickly adapt when moving between tech stacks or job roles.
+            </p>
+          </div>
+          <div className="card">
+            <h1>
+              Soft skills
+            </h1>
+            <div className="List-segragation">
+              <ul>
+                <li>Adaptive</li>
+                <li>Communication</li>
+                <li>Cooperation</li>
+                <li>Creativity</li>
+                <li>Dependability</li>
+                <li>Handling pressure</li>
+                <li>Leadership</li>
+                <li>Organisation/Planning</li>
+                <li>Problem-solving</li>
+                <li>Time Keeping</li>
+                <li>Teaching</li>   
+              </ul>
+            </div>
+          </div>
+        </div> 
+    )
+}
